efb5cd99c1b531d6b943420f15fc0b2778865620
Jared Peterson <hobbitcakes@gmail.com>
2022-01-30 14:47:22 -0600

    Removed dead code. Readme update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # discord-dice
+### To add this bot:
+https://discord.com/api/oauth2/authorize?client_id=874624607007703111&scope=bot&permissions=277025602624
--- main.py
+++ main.py
@@ -14,14 +14,6 @@ def load_token(file: str) -> str:
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    
-#@client.event
-#async def on_message(message):
-#    if message.author == client.user:
-#        return
-#    if message.content.startswith('$hello'):
-#        logging.info('Got a request!')
-#        await message.channel.send('Hello!')
 
 @client.command()
 async def roll(ctx, *args):
